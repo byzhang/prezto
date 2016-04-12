cae7cab84e14abd670d573d8cfc5435067f9ed84
Benyu Zhang <byzhang@gmail.com>
2016-04-12 16:19:32 -0700

    Change the softlinks to relative paths.

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #/usr/bin/zsh
 
-git clone --recursive https://github.com/byzhang/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+cd
+git clone --recursive https://github.com/byzhang/prezto.git "${ZDOTDIR:-.}/.zprezto"
 
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+for rcfile in "${ZDOTDIR:-.}"/.zprezto/runcoms/^README.md(.N); do
+  ln -sf "$rcfile" "${ZDOTDIR:-.}/.${rcfile:t}"
 done
 
 chsh -s /usr/bin/zsh
